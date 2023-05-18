a84dfc7e72582aabead90aaba6795c1a6b8c2e62
dyukoffv4 <net998art@gmail.com>
2023-05-18 08:37:03 +0300

    Tests add

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-ML_HOST = os.getenv('ML_HOST', '127.0.0.1')
+ML_HOST = os.getenv('ML_HOST', 'localhost')
 ML_PORT = os.getenv('ML_PORT', 8080)
-XQ_HOST = os.getenv('XQ_HOST', '127.0.0.1')
+XQ_HOST = os.getenv('XQ_HOST', 'localhost')
 XQ_PORT = os.getenv('XQ_PORT', 18040)
 XQ_USERNAME = os.getenv('XQ_USERNAME', 'ml_service')
 XQ_PASSWORD = os.getenv('XQ_PASSWORD', 'ml_password')
--- xqueue.py
+++ xqueue.py
@@ -1,10 +1,14 @@
+import os
 import json
 import pprint
 import uvicorn
 from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse, HTMLResponse
 from config import XQ_HOST, XQ_PORT, XQ_USERNAME, XQ_PASSWORD
+from test import Tests
 
 app = FastAPI()
+tests = Tests('tests', XQ_HOST + ':' + str(XQ_PORT))
 
 
 @app.post('/xqueue/login/')
@@ -17,31 +21,12 @@ def xqueue_login(username=Form(), password=Form()):
 @app.get('/xqueue/get_submission/')
 def xqueue_get_submission(queue_name: str):
     pprint.pprint(queue_name)
-    x_header = {
-        'submission_id': '<id>',
-        'submission_key': '<key>'
-    }
-    x_body = {
-        'token': 'token',
-        'data': {
-            'student_data': [
-                'http://video.mp4',
-            ],
-            'student_info': [
-                'http://photo.jpeg'
-            ]
-        }
-    }
-    x_files = {
-        'anything': ''
-    }
-    data = {
-        'xqueue_header': json.dumps(x_header),
-        'xqueue_body': json.dumps(x_body),
-        'xqueue_files': json.dumps(x_files)
-    }
     # ToDo: send to test
-    return {'content': json.dumps(data)}
+    data = tests.curr()
+    if data is not None:
+        tests.next()
+        return {'content': json.dumps(data)}
+    return {'content': {}}
 
 
 @app.post('/xqueue/put_result/')
@@ -52,6 +37,14 @@ def xqueue_put_result(xqueue_header=Form(), xqueue_body=Form()):
     pprint.pprint(f'xqueue_header:\n{xqueue_header}')
     pprint.pprint(f'xqueue_body:\n{xqueue_body}')
     return {'result_code': 0}
+
+
+@app.get('/tests/{test_id}/data/{file_name}')
+def test_data_send(test_id: int, file_name: str):
+    pprint.pprint(f'{test_id}, {file_name}')
+    if os.path.exists(f'tests/{test_id}/data/{file_name}'):
+        return FileResponse(f'tests/{test_id}/data/{file_name}', media_type='application/octet-stream')
+    return HTMLResponse(status_code=404)
 
 
 if __name__ == '__main__':
